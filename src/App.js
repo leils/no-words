8969e9c39ff6b435d98863bf88c2fcf01925b512
Leia <e.leiachang@gmail.com>
2022-03-01 12:43:21 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function TalkButton(props) {
+  return <button type="button">Talk?</button>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TalkButton/>
       </header>
     </div>
   );
