aa8c7f0a699c7b039042b44084f69f6498f36f5c
Leia <e.leiachang@gmail.com>
2022-03-01 12:59:00 -0500

    iterating through words

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSpeechSynthesis } from 'react-speech-kit';
+import Words from './words';
 
 function TalkButton(props) {
   const { speak } = useSpeechSynthesis();
   return(
     <button
       type="button"
-      onClick={() => speak({ text: "Talking" })}>
-      Talk?
+      onClick={() => speak({ text: props.word })}>
+      {props.word}
     </button>)
 }
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TalkButton/>
+        { Words.map((word) => <TalkButton word={word}/>)}
+        
+        <TalkButton word="Talking"/>
       </header>
     </div>
   );
